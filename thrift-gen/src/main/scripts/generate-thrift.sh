df85aebb65db53d3da7db2ca334bdf0f34a6217b
Trent Nadeau <tnadeau@42six.com>
2015-02-10 14:37:25 -0500

    Added thrift generation scripts as an artifact

---
+++ thrift-gen/src/main/scripts/generate-thrift.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+#   Copyright (C) 2013-2014 Computer Sciences Corporation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+set -e
+
+function gen_thrift () {
+    thrift_src="$1"
+    language_name=$2
+    language_gen_suffix=$3
+    language_opt=$4
+
+    thrift_gen="$thrift_src/../../../target/thrift-gen/$language_gen_suffix"
+    artifact_base="${thrift_src}/../../../"
+
+    rm -rf "${thrift_gen}"
+    mkdir -p "${thrift_gen}"
+
+    common_args="-I $thrift_src "
+    common_args+="-I ${artifact_base}/target/thrift-deps/src/main/thrift "
+
+    for thrift_file in "${thrift_src}"/*.thrift; do
+        if [[ ${language_name} == "Go" && -z "$(type -P gofmt)" ]]; then
+            echo "[WARN] Skipping Go because it is not installed or on the path"
+        else
+            echo "Generating $language_name code for $(basename ${thrift_file})"
+            thrift ${common_args} --gen ${language_opt} -out "${thrift_gen}" "${thrift_file}"
+        fi
+    done
+
+    if [[ ${language_name} == "C++" ]]; then
+        echo "Moving $language_name include files to include directory"
+        mkdir "${thrift_gen}/include"
+        mv "${thrift_gen}"/*.h "${thrift_gen}"/include
+
+        echo "Removing skeleton files"
+        rm -f "${thrift_gen}"/*.skeleton.cpp
+    elif [[ ${language_name} == "Python" ]]; then
+        # delete top level __init__.py
+        rm -f "${thrift_gen}"/__init__.py
+        ./generate_setup.py -m "${artifact_base}"
+    fi
+}
+
+declare -a GENS=(\
+    "Java java java:hashcode" \
+    "C++ cpp cpp:cob_style" \
+    "Python python py:new_style" \
+    "NodeJS node js:node" \
+    "Go go go")
+
+THRIFT_REQ_VERSION="0.9.1"
+
+#If generation_mode is "all", the script will fail if all the supported languages cannot be generated
+#Any other value will generate whatever languages it can, outputting a warning for languages that cannot be generated
+#Currently, Go is the only language with special requirements
+generation_mode=$1
+
+if [[ -z "$(type -P thrift)" ]]; then
+    echo "Thrift ${THRIFT_REQ_VERSION} must be installed and on the PATH" >&2
+    exit 1
+fi
+
+thrift_version_str="$(thrift --version)" || true
+thrift_version="${thrift_version_str##* }"
+if [[ "${thrift_version}" != "${THRIFT_REQ_VERSION}" ]]; then
+    echo "Thrift is installed and on the PATH but is not version ${THRIFT_REQ_VERSION}" >&2
+    echo "Thrift version found: ${thrift_version}" >&2
+    exit 1
+fi
+
+if [[ generation_mode == "all" && -z "$(type -P gofmt)" ]]; then
+    echo "Go (including gofmt) must be installed and on the PATH" >&2
+    echo "This can be installed using Homebrew with 'brew install go'" >&2
+    exit 1
+fi
+
+DIR=${2-.}
+
+for thrift_src in $(find ${DIR} -path "*/src/main/thrift" -a ! -path "*target*"); do
+    thrift_dir="$(cd "${thrift_src}" && pwd -P)"
+
+    echo "Going into $(dirname ${thrift_dir})"
+
+    for gen in "${GENS[@]}"; do
+        gen_thrift "${thrift_src}" ${gen}
+    done
+done
---
+++ thrift-gen/src/main/scripts/generate_setup.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+#   Copyright (C) 2013-2014 Computer Sciences Corporation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import os
+import xml.etree.ElementTree as ET
+import argparse
+
+MODULE_DIR = 'module_dir'
+NAMESPACE_PKGS = 'namespace_packages'
+NAMESPACE_PKG_CONTENTS = "__import__('pkg_resources').declare_namespace(__name__)\n"
+COMMON_DEPS = ['thrift==0.9.1']
+PY_DEV_VERSION = "rc1.dev"
+
+
+def make_namespace_packages(start, basepkg=None):
+    packages = []
+    for dirname, dirnames, filenames in os.walk(start):
+        if basepkg and not dirname.startswith(os.path.join(start, basepkg)):
+            continue
+        if len(filenames) == 1 and '__init__.py' in filenames:
+            package = dirname[len(start + os.path.sep):]
+            if package:
+                with open(os.path.join(dirname, '__init__.py'), 'wb') as f:
+                    f.write(NAMESPACE_PKG_CONTENTS)
+                packages.append(package.replace(os.path.sep, '.'))
+    return packages
+
+
+def find_all_packages(start, basepkg=None):
+    packages = {}
+    for dirname, dirnames, filenames in os.walk(start):
+        if dirname.endswith('target/thrift-gen/python'):
+            packages[dirname] = {
+                MODULE_DIR: dirname[:dirname.find('/target/thrift-gen/python')],
+                NAMESPACE_PKGS: make_namespace_packages(dirname, basepkg)
+            }
+            del dirnames[:]
+
+    return packages
+
+
+def write_setup_file(output_dir, pkg_name, pkg_ver, additional_args=None):
+    if additional_args is None:
+        additional_args = []
+
+    template_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "setup.py.template")
+    setup_py_file = os.path.join(os.path.abspath(output_dir), "setup.py")
+
+    with open(template_file, 'r') as inf:
+        template = inf.read()
+
+    with open(setup_py_file, 'w+') as outf:
+        outf.write(template.format(PACKAGE_NAME=pkg_name,
+                                   PACKAGE_VERSION=pkg_ver,
+                                   PACKAGE_ADDITIONAL_SETUP_ARGS=',\n    '.join(additional_args)))
+
+
+def parse_pom(pom):
+    artifact_name = None
+    artifact_version = None
+
+    tree = ET.iterparse(pom)
+    for _, el in tree:
+        el.tag = el.tag.split('}', 1)[1]
+    root = tree.root
+
+    # determine artifact name
+    for element in root.findall('./artifactId'):
+        artifact_name = element.text
+        break
+
+    # determine version number
+    for element in root.findall('./version'):
+        artifact_version = element.text
+        break
+    if not artifact_version:
+        # take version from the parent
+        for version in root.findall('./parent/version'):
+            artifact_version = version.text
+            break
+
+    # determine dependencies
+    dependencies = []
+    for element in root.findall('./dependencies/dependency/artifactId'):
+        dependencies.append(element.text)
+
+    return artifact_name, artifact_version, dependencies
+
+
+def maven_version_to_py(maven_version):
+    version = maven_version
+    if '-' in maven_version:
+        version, specifier = maven_version.split('-')
+        if specifier == 'SNAPSHOT':
+            version = "{0}{1}".format(version, PY_DEV_VERSION)
+    return version
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--module', default='.', help='module to generate setup.py for')
+
+    return parser.parse_args()
+
+
+def main(args):
+    packages = find_all_packages(args.module, 'ezbake')
+
+    for path, data in packages.iteritems():
+        module_dir = data[MODULE_DIR]
+        namespace_packages = data[NAMESPACE_PKGS]
+        pkg_name, pkg_version, dependencies = parse_pom(os.path.join(module_dir, 'pom.xml'))
+        pkg_version = maven_version_to_py(pkg_version)
+        dependencies = ["{0}>={1}".format(x, pkg_version) for x in dependencies]
+        dependencies.extend(COMMON_DEPS)
+
+        print("Generating setup.py for {0}. Name: {1}, Version: {2}".format(
+            module_dir, pkg_name, pkg_version))
+
+        additional_params = [
+            "install_requires={0}".format(dependencies),
+            "namespace_packages={0}".format(namespace_packages)
+        ]
+        write_setup_file(path, pkg_name, pkg_version, additional_params)
+
+
+if __name__ == '__main__':
+    main(parse_arguments())
+
