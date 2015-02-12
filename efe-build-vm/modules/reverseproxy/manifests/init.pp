5de972c6a3b6740a6a5da866628028dfd6e68100
bhammen <bhammen@42six.com>
2015-02-12 11:23:25 -0500

    Add license headers

--- efe-build-vm/modules/reverseproxy/manifests/init.pp
+++ efe-build-vm/modules/reverseproxy/manifests/init.pp
@@ -1,3 +1,17 @@
+/*   Copyright (C) 2013-2014 Computer Sciences Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
 class reverseproxy {
   $build_yum = [ "boost", "boost-devel", "python-devel", "libevent", "libevent-devel", "pcre", "pcre-devel", "readline-devel", "mlocate", "vim-enhanced", "tree", "libedit", "libtool", "byacc", "flex", "apr", "apr-devel", "apr-util", "apr-util-devel", ]
 
