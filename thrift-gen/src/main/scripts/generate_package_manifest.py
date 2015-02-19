5ef0e0f25c856c8a84d236a55e6efb6f30e8e935
Jeff Hastings <jhastings@42six.com>
2015-02-19 18:06:50 -0500

    Added support for node packages to the now-renamed generate_setup.py

--- thrift-gen/src/main/scripts/generate_package_manifest.py
+++ thrift-gen/src/main/scripts/generate_package_manifest.py
@@ -34,11 +34,14 @@ except ImportError:
 
 
 def make_namespace_packages(start, basepkg=None):
+    """Finds all packages that should be namespace packages
+    This includes everything that only has an __init__.py file"""
     packages = []
     for dirname, dirnames, filenames in os.walk(start):
         if basepkg and not dirname.startswith(os.path.join(start, basepkg)):
             continue
-        if len(filenames) == 1 and '__init__.py' in filenames:
+        py_files = [x for x in filenames if x.endswith('.py')]
+        if len(py_files) == 1 and '__init__.py' in filenames:
             package = dirname[len(start + os.path.sep):]
             if package:
                 with open(os.path.join(dirname, '__init__.py'), 'wb') as f:
@@ -47,25 +50,34 @@ def make_namespace_packages(start, basepkg=None):
     return packages
 
 
-def find_all_packages(start, basepkg=None):
+def find_all_packages(start, type):
+    """Finds all target/thrift-gen/{type} packages under start directory"""
+    package_dir_name = 'target/thrift-gen/{0}'.format(type)
     packages = {}
     for dirname, dirnames, filenames in os.walk(start):
-        if dirname.endswith('target/thrift-gen/python'):
+        if dirname.endswith(package_dir_name):
             packages[dirname] = {
-                MODULE_DIR: dirname[:dirname.find('/target/thrift-gen/python')],
-                NAMESPACE_PKGS: make_namespace_packages(dirname, basepkg)
+                MODULE_DIR: dirname[:dirname.find(package_dir_name)],
+                #NAMESPACE_PKGS: make_namespace_packages(dirname, basepkg)
             }
             del dirnames[:]
 
     return packages
 
 
-def write_setup_file(output_dir, pkg_name, pkg_ver, additional_args=None):
+def write_package_file(type, output_dir, pkg_name, pkg_ver, additional_args=None):
+    """Reads the template file for the given type and writes out the package file (setup.py, package.json, etc)"""
     if additional_args is None:
         additional_args = []
 
-    template_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "setup.py.template")
-    setup_py_file = os.path.join(os.path.abspath(output_dir), "setup.py")
+    if type == 'python':
+        template_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "setup.py.template")
+        setup_py_file = os.path.join(os.path.abspath(output_dir), "setup.py")
+    elif type == 'node':
+        template_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "package.json.template")
+        setup_py_file = os.path.join(os.path.abspath(output_dir), "package.json")
+    else:
+        raise ValueError("Type must be on of python or node")
 
     with open(template_file, 'r') as inf:
         template = inf.read()
@@ -77,6 +89,7 @@ def write_setup_file(output_dir, pkg_name, pkg_ver, additional_args=None):
 
 
 def parse_pom(pom):
+    """Parse a pom file and get out artifact name, version, and dependencies"""
     artifact_name = None
     artifact_version = None
 
@@ -108,42 +121,58 @@ def parse_pom(pom):
     return artifact_name, artifact_version, dependencies
 
 
-def maven_version_to_py(maven_version):
-    version = maven_version
-    if '-' in maven_version:
-        version, specifier = maven_version.split('-')
-        if specifier == 'SNAPSHOT':
-            version = "{0}{1}".format(version, PY_DEV_VERSION)
+def version_for_package_type(type, version):
+    """Get the appropriate version for the given type from a maven version number"""
+    if type == 'python':
+        if '-' in version:
+            version, specifier = version.split('-')
+            if specifier == 'SNAPSHOT':
+                version = "{0}{1}".format(version, PY_DEV_VERSION)
+    elif type == 'node':
+        version_suffix = ''
+        if '-' in version:
+            # handle snapshot
+            version, specifier = version.split('-')
+            version_suffix = "-snapshot"
+        versions = version.split('.')
+        if len(versions) < 3:
+            versions.append(0)
+        version = '.'.join(str(x) for x in versions) + version_suffix
     return version
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--module', default='.', help='module to generate setup.py for')
-
+    parser.add_argument('-t', '--type', default='python', choices=['python', 'node'], help='type of package to generate')
     return parser.parse_args()
 
 
 def main(args):
-    packages = find_all_packages(args.module, 'ezbake')
+    packages = find_all_packages(args.module, args.type)
 
     for path, data in packages.iteritems():
         module_dir = data[MODULE_DIR]
-        namespace_packages = data[NAMESPACE_PKGS]
         pkg_name, pkg_version, dependencies = parse_pom(os.path.join(module_dir, 'pom.xml'))
-        pkg_version = maven_version_to_py(pkg_version)
+        pkg_version = version_for_package_type(args.type, pkg_version)
         dependencies = ["{0}>={1}".format(x, pkg_version) for x in dependencies if x not in EXCLUDED_DEPS]
         dependencies.extend(COMMON_DEPS)
 
-        print("Generating setup.py for {0}. Name: {1}, Version: {2}".format(
-            module_dir, pkg_name, pkg_version))
+        additional_params = []
+        if args.type == 'python':
+            namespace_packages = make_namespace_packages(path, 'ezbake')
+            additional_params = [
+                "install_requires={0}".format(dependencies),
+                "namespace_packages={0}".format(namespace_packages)
+            ]
+            write_package_file(args.type, path, pkg_name, pkg_version, additional_params)
+        elif args.type == 'node':
+            additional_params = [
+                ",\"dependencies\": {0}".format(str(COMMON_DEPS).replace('\'', '"'))
+            ]
 
-        additional_params = [
-            "install_requires={0}".format(dependencies),
-            "namespace_packages={0}".format(namespace_packages)
-        ]
-        write_setup_file(path, pkg_name, pkg_version, additional_params)
-
+        print("Generating package for {0}. Name: {1}, Version: {2}".format(module_dir, pkg_name, pkg_version))
+        write_package_file(args.type, path, pkg_name, pkg_version, additional_params)
 
 if __name__ == '__main__':
     main(parse_arguments())
